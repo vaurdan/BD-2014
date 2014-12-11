6e75a2311f37bbed27e2d300988a4f1f44621167
Henrique Mouta <henrique@mowta.pt>
2014-12-11 22:52:25 +0000

    Corrigido identacao

--- estrela.sql
+++ estrela.sql
@@ -1,16 +1,16 @@
 
 DROP TABLE IF EXISTS dimensao_tempo ;
 CREATE TABLE dimensao_tempo(
-	tempo_id 		INT AUTO_INCREMENT,
-	dia         INT NOT NULL,
-	mes         INT NOT NULL,
-	ano         INT NOT NULL,
-	PRIMARY KEY (tempo_id)
+  tempo_id    INT AUTO_INCREMENT,
+  dia         INT NOT NULL,
+  mes         INT NOT NULL,
+  ano         INT NOT NULL,
+  PRIMARY KEY (tempo_id)
 );
 
 DROP TABLE IF EXISTS dimensao_local ;
 CREATE TABLE dimensao_local(
-  local_id 		INT AUTO_INCREMENT,
+  local_id    INT AUTO_INCREMENT,
   regiao      VARCHAR(80) NOT NULL,
   concelho    VARCHAR(80) NOT NULL,
   PRIMARY KEY (tempo_id)
@@ -18,14 +18,14 @@ CREATE TABLE dimensao_local(
 
 DROP TABLE IF EXISTS maior_lance ;
 CREATE TABLE maior_lance(
-	tempo_id 		INT NOT NULL,
-	local_id    INT NOT NULL,
-	nif         INT NOT NULL,
-	lid         INT NOT NULL,
-	valor       INT NOT NULL,
+  tempo_id    INT NOT NULL,
+  local_id    INT NOT NULL,
+  nif         INT NOT NULL,
+  lid         INT NOT NULL,
+  valor       INT NOT NULL,
   FOREIGN KEY (tempo_id) REFERENCES dimensao_tempo(tempo_id),
   FOREIGN KEY (local_id) REFERENCES dimensao_local(local_id),
   FOREIGN KEY (nif) REFERENCES lance(pessoa),
-	FOREIGN KEY (lid) REFERENCES lance(leilao),
-	FOREIGN KEY (valor) REFERENCES lance(valor)
+  FOREIGN KEY (lid) REFERENCES lance(leilao),
+  FOREIGN KEY (valor) REFERENCES lance(valor)
 );
